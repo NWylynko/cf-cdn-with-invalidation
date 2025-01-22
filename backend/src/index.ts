9440e86ebf3d7081a0be3ff307dfc1e22f5fc842
Nick Wylynko <nick@wylynko.com>
2025-01-23 03:16:46 +0800

    add in cache tagging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { db, driz, schema } from "./database.js"
+import Cloudflare from 'cloudflare';
+import { z } from "zod";
 
 const app = fastify({ logger: true });
 
@@ -12,7 +14,20 @@ app.get("/", async (req, res) => {
     .select()
     .from(schema.dataTable)
 
-  return { result: data };
+  res
+    .status(200)
+    .header("Cache-Tag", "data")
+    .header("Content-Type", "application/json")
+    .send(data)
+});
+
+const env = z.object({
+  CF_API_TOKEN: z.string().url(),
+  CF_ZONE_ID: z.string().url(),
+}).parse(process.env)
+
+const cloudflare = new Cloudflare({
+  apiToken: env.CF_API_TOKEN,
 });
 
 app.post("/", async (req, res) => {
@@ -21,8 +36,13 @@ app.post("/", async (req, res) => {
     .update(schema.dataTable)
     .set({ number: Math.floor(Math.random() * 100_000_000) })
 
-  // call back to cf to invalidate cache
+  await cloudflare.cache.purge({
+    zone_id: env.CF_ZONE_ID,
+    tags: ["data"],
+  })
 
+  res.status(200).send("ok")
 });
 
 await app.listen({ port: 4000, host: "0.0.0.0" })
+
